00ce73e070b937ae049173898303702d971a3849
joeyjoy007 <garvitjain5558@gmail.com>
2022-08-31 18:23:42 +0530

    typeScript Array

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-var typeScriptObject = {
+// const typeScriptObject:{
+//     name:string,
+//     age:number
+// } = {
+//     name:"Garvit",
+//     age:20
+// }
+// console.log(typeScriptObject.name)
+var arrayObject = {
     name: "Garvit",
-    age: 20
+    age: 30,
+    hobies: ["Sports", "Coding"]
 };
-console.log(typeScriptObject.name);
+console.log(arrayObject.hobies);
+for (var _i = 0, _a = arrayObject.hobies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log("Hobbies are", hobby);
+}
--- app.ts
+++ app.ts
@@ -1,8 +1,25 @@
-const typeScriptObject:{
+// const typeScriptObject:{
+//     name:string,
+//     age:number
+// } = {
+//     name:"Garvit",
+//     age:20
+// }
+// console.log(typeScriptObject.name)
+
+const arrayObject:{
     name:string,
-    age:number
-} = {
+    age:number,
+    hobies:string[]
+
+}={
     name:"Garvit",
-    age:20
+    age:30,
+    hobies:["Sports","Coding"]
 }
-console.log(typeScriptObject.name)
+
+console.log(arrayObject.hobies)
+
+for(const hobby of arrayObject.hobies){
+    console.log("Hobbies are",hobby)
+}
