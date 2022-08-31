e043cd5a426f0c58d5e9d31201413ed03263cd20
joeyjoy007 <garvitjain5558@gmail.com>
2022-08-31 18:11:21 +0530

    typeScript Objects

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-function add(n1, n2) {
-    return n1 + n2;
-}
-var number1 = 4;
-var number2 = 2;
-var result = add(number1, number2);
-console.log(result);
+var typeScriptObject = {
+    name: "Garvit",
+    age: 20
+};
+console.log(typeScriptObject.name);
