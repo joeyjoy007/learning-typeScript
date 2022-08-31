842f32fac6ca19d1bcd03fe799d2104a7ec3ffa5
joeyjoy007 <garvitjain5558@gmail.com>
2022-08-31 10:32:47 +0530

    typecript funnction

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("your code goes here okay bhai");
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 4;
+var number2 = 2;
+var result = add(number1, number2);
+console.log(result);
